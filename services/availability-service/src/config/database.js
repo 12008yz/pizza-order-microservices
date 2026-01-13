56e62054dfdbdfdbc9704762ab5ee44b5ecb6d3a
Денис <deniscikasov@gmail.com>
2026-01-13 19:16:36 +0300

    инфраструктура. фаза 1 (+-)

--- services/availability-service/src/config/database.js
+++ services/availability-service/src/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD || 'availability_password',
     database: process.env.DB_NAME || 'availability_db',
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
+    port: parseInt(process.env.DB_PORT || '5437'),
     dialect: 'postgres',
   },
   production: {
@@ -14,7 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432'),
+    port: parseInt(process.env.DB_PORT || '5437'),
     dialect: 'postgres',
   },
 };
